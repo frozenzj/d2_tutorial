ad79d1a3b2270fdac08ea8b2a75e58c37d0768d6
frozenzj <39781749+frozenzj@users.noreply.github.com>
2018-06-04 23:09:11 +0800

    增加金钱汇总
    汇总某场次金钱获得数，可细化至个人

--- dota2_api.py
+++ dota2_api.py
@@ -11,7 +11,7 @@ def get_api_json(url):
 if __name__ == '__main__':
     pass
 def get_ran_match_id(limi=1):
-    
+
 #    def ransql(limi=1):
         sql='''select matches.match_id
         from matches
@@ -22,3 +22,13 @@ def get_ran_match_id(limi=1):
         r_json=r.json()
         rMi=r_json['rows']
         return rMi
+#TEAM_GOLD
+#match_info = apimatch.json()    slot_s = start slot num.   slot_e = end slot num.
+def get_totalgold(match_info,slot_s=0,slot_e=5):
+    i=0
+    j=0
+    k=0
+    for i in range(slot_s,slot_e):
+        k=match_info['players'][i]['total_gold']
+        j=j+k
+    return j
